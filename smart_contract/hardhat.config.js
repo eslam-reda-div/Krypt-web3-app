618a229393658b75d7a4a0373392615303f4ec0a
eslam reda <eslam.reda.div@gmail.com>
2024-01-06 10:06:38 +0200

    test

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-mainnet.g.alchemy.com/v2/QvyNaRhOMGQeNoy9Cbj5F0Gw65O2TczW',
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
       accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
     },
   },
